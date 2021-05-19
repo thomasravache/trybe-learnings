8c297c1ddc6be4dd8fd19dd4aa50daf8279052a0
thomasravache <thomasravache31@gmail.com>
2021-05-19 16:25:56 -0300

    Exercícios da parte 3 do conteúdo --> Finalizado.

---
+++ fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/conteudo/exercicio2-2.js
@@ -0,0 +1,8 @@
+let base = 5;
+let altura = 8;
+
+let area = base * altura;
+console.log("Área do retangulo é: " + area);
+
+let perimetro = base * 2 + altura * 2;
+console.log("O perímetro é: " + perimetro);
