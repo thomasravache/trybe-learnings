5d788c7eac4fb407dd43a1d3397bd45de27d22d9
thomasravache <thomasravache31@gmail.com>
2021-05-19 20:03:03 -0300

    Exercício 04 --> Finalizado.

---
+++ fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia1-primeiros-passos/exercises/exercise4.js
@@ -0,0 +1,16 @@
+// Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+let status = 'outro';
+
+switch (status) {
+  case 'positive':
+  console.log('positive');
+  break;
+
+  case 'negative':
+  console.log('negative');
+  break;
+
+  default:
+    console.log('zero');
+}
