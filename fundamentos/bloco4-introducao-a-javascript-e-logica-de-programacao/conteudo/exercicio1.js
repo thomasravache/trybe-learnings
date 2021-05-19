9a9f59f4370c77e3d148c9e412ab2963bc36825a
thomasravache <thomasravache31@gmail.com>
2021-05-19 15:37:35 -0300

    Exercício Parte ll do conteúdo --> Finalizado.

---
+++ fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/conteudo/exercicio1.js
@@ -0,0 +1,11 @@
+const myName = "Thomas";
+const birthCity = "São Paulo";
+const birthYear = 1998;
+
+birthYear = 2030; //Vai dar erro por conta de ser uma constante!
+birthCity = "Rio de Janeiro";
+
+
+console.log("Meu nome é " + myName);
+console.log("Nascido em: " + birthCity);
+console.log("Ano de nascimento: " + birthYear);
