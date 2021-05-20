2cc1c4dcbc141479832ac8ef2032a0e3d2e4b2f7
thomasravache <thomasravache31@gmail.com>
2021-05-20 18:48:02 -0300

    Exercício 09 --> Finalizado.

---
+++ fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia2-array-e-loop-for/exercises/exercise08.js
@@ -0,0 +1,9 @@
+// Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+
+let arrayOfNumbers = [];
+
+for (let newValues = 1; newValues <= 25; newValues+=1){
+  arrayOfNumbers.push(newValues);
+}
+
+console.log(arrayOfNumbers);
