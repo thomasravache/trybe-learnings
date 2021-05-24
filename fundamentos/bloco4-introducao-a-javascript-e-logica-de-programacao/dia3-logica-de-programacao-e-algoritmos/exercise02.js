79553dbf50f0e2a4f9b2f2efe721fca14e1270e7
thomasravache <thomasravache31@gmail.com>
2021-05-24 12:59:50 -0300

    Exercício 02 --> Finalizado

---
+++ fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia3-logica-de-programacao-e-algoritmos/exercise02.js
@@ -0,0 +1,18 @@
+// Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+// n = 5
+
+// *
+// **
+// ***
+// ****
+// *****
+
+let n = 5;
+let text = '';
+
+for (let line = 1; line <= n; line+=1){
+  
+  text += '*';
+  console.log(text);
+
+}
