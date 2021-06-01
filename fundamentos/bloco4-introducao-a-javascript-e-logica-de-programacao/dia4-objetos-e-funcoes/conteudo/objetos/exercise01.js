98319024a2e35f3c7ecd7222377a94ee02127ecc
thomasravache <thomasravache31@gmail.com>
2021-06-01 13:07:24 -0300

    Exercícios da parte 1 do conteúdo --> Finalizado.

---
+++ fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia4-objetos-e-funcoes/conteudo/objetos/exercise01.js
@@ -0,0 +1,19 @@
+// Crie um objeto player contendo as variáveis listadas abaixo.
+// Copiar
+// let name = 'Marta';
+// let lastName = 'Silva';
+// let age = 34;
+// let medals = { golden: 2, silver: 3 };
+
+
+let player = {
+
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: {
+    golden: 2,
+    silver: 3
+  }
+
+}
