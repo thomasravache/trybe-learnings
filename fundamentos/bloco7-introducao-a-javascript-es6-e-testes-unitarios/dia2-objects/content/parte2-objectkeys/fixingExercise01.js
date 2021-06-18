cefc4c59d8bfd86062129187fb28526dc9a1bafa
thomasravache <thomasravache31@gmail.com>
2021-06-18 15:45:02 -0300

    Exercicio fixaçao 01 OK

---
+++ fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia2-objects/content/parte2-objectkeys/fixingExercise01.js
@@ -0,0 +1,18 @@
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+for (let keys in student1) {
+  console.log(`${keys}, Nível: ${student1[keys]}`);
+};
