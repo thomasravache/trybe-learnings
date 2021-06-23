0b84eafbef537b506e6ae8cc8f45c06de40408e9
thomasravache <thomasravache31@gmail.com>
2021-06-23 15:57:30 -0300

    Parte 1 do exercicio de fixacao OK

--- fundamentos/bloco8-higher-order-functions-do-javascript-ES6/dia1-javascript-es6-introducao-a-higher-order-functions/content/fixingExercise01.js
+++ fundamentos/bloco8-higher-order-functions-do-javascript-ES6/dia1-javascript-es6-introducao-a-higher-order-functions/content/fixingExercise01.js
@@ -1,4 +1,10 @@
 // Ao chamar a função doingThings:
+const doingThings = (func) => console.log(func());
+
+const wakeUp = () => `Acordando!!`;
+
+
+
 doingThings(wakeUp);
 
 // Ela deve retornar o valor do respectivo parâmetro, neste caso:
