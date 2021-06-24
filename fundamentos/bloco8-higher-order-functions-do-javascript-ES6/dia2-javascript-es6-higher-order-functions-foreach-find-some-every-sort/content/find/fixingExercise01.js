2c157dc502876e1b1a630cc60a25b60673164d3c
thomasravache <thomasravache31@gmail.com>
2021-06-24 18:21:53 -0300

    Exercicio de fixacao 01 da parte de find - OK

--- fundamentos/bloco8-higher-order-functions-do-javascript-ES6/dia2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/content/find/fixingExercise01.js
+++ fundamentos/bloco8-higher-order-functions-do-javascript-ES6/dia2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/content/find/fixingExercise01.js
@@ -1,7 +1,7 @@
 const numbers = [19, 21, 30, 3, 45, 22, 15];
 
-const findDivisibleBy3And5 = () => {
-  // Adiciona seu código aqui
+const findDivisibleBy3And5 = (arrayOfNumbers) => {
+  return arrayOfNumbers.find((number) => number % 3 === 0 && number % 5 === 0);
 }
 
-console.log(findDivisibleBy3And5())
+console.log(findDivisibleBy3And5(numbers));
