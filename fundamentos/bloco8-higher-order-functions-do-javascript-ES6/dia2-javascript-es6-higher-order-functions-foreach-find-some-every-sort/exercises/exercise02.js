ac1809e96bd9577b54acd848fb14c2f9fb81c354
thomasravache <thomasravache31@gmail.com>
2021-06-24 22:22:21 -0300

    Exercicio 02 - OK

--- fundamentos/bloco8-higher-order-functions-do-javascript-ES6/dia2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercises/exercise02.js
+++ fundamentos/bloco8-higher-order-functions-do-javascript-ES6/dia2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercises/exercise02.js
@@ -65,9 +65,13 @@ const books = [
 
 // Adicione o código do exercício aqui:
 function smallerName() {
-  let nameBook;
+  let nameBook = '';
   // escreva aqui o seu código
-
+  books.forEach((book) => {
+    if (nameBook === '' || book.name.length < nameBook.length) {
+    nameBook = book.name;
+    }
+  });
   // Variável nameBook que receberá o valor do menor nome;
   return nameBook;
 }
