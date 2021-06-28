c52b75496016aea0f4c1e18988c156d8a52b61a5
thomasravache <thomasravache31@gmail.com>
2021-06-28 16:31:21 -0300

    Adicionando mais alteraçoes ao exercicio 02 de fixacao

--- fundamentos/bloco8-higher-order-functions-do-javascript-ES6/dia4-javascript-es6-higher-order-functions-reduce/content/fixingExercise02.js
+++ fundamentos/bloco8-higher-order-functions-do-javascript-ES6/dia4-javascript-es6-higher-order-functions-reduce/content/fixingExercise02.js
@@ -1,7 +1,9 @@
 const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
 
 const getEvenNumbers = (evenNumber) => evenNumber % 2 === 0;
+const sumEven = (accumulator, value) => accumulator + value;
 // Filtra o array pegando armazenando em um novo array apenas os pares e depois soma todos esses valores com o reduce
-const sumEvenNumbers = numbers.filter(getEvenNumbers).reduce((accumulator, value) => accumulator += value);
 
-console.log(sumEvenNumbers);
+const sumEvenNumbers = (array) => array.filter(getEvenNumbers).reduce(sumEven);
+
+console.log(sumEvenNumbers(numbers));
