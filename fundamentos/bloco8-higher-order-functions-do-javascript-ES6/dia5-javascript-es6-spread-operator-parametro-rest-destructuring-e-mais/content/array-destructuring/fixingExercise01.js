8fd1f57eae4696c7349ada713ccc211df1f739a2
thomasravache <thomasravache31@gmail.com>
2021-06-29 18:57:49 -0300

    Exercicio de fixacao 01 array destructuring - OK

--- fundamentos/bloco8-higher-order-functions-do-javascript-ES6/dia5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/content/array-destructuring/fixingExercise01.js
+++ fundamentos/bloco8-higher-order-functions-do-javascript-ES6/dia5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/content/array-destructuring/fixingExercise01.js
@@ -3,3 +3,7 @@ const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
 saudacoes[1](saudacoes[0]); // Olá
 
 // Produza o mesmo resultado acima, porém utilizando array destructuring
+
+const [text, func] = saudacoes;
+
+func(text);
