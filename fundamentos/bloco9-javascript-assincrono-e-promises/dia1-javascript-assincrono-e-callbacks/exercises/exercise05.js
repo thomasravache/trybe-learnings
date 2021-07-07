507b4ce546acaee936b0578c27a279f68f29f333
thomasravache <thomasravache31@gmail.com>
2021-07-07 10:42:46 -0300

    Exercicio 05 - Resolvido

--- fundamentos/bloco9-javascript-assincrono-e-promises/dia1-javascript-assincrono-e-callbacks/exercises/exercise05.js
+++ fundamentos/bloco9-javascript-assincrono-e-promises/dia1-javascript-assincrono-e-callbacks/exercises/exercise05.js
@@ -11,6 +11,11 @@ const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now 
 
 // definição da função sendMarsTemperature...
 
+const sendMarsTemperature = (callback) => {
+  const temperature = getMarsTemperature();
+  const delay = messageDelay();
+  setTimeout(() => callback(temperature), delay);
+}
 
 sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
 sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
