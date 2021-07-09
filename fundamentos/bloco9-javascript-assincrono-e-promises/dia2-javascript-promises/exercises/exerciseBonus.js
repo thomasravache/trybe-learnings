1ad0924febaa6eab974e93d65e593d89924ce86b
thomasravache <thomasravache31@gmail.com>
2021-07-09 20:25:44 -0300

    Exercicio bonus - Resolvido

--- fundamentos/bloco9-javascript-assincrono-e-promises/dia2-javascript-promises/exercises/exerciseBonus.js
+++ fundamentos/bloco9-javascript-assincrono-e-promises/dia2-javascript-promises/exercises/exerciseBonus.js
@@ -1,27 +1,32 @@
-const returnArrayDivision = (result) => {
+const sumRandomNumbers = () => {
+  const array = Array
+  .from(
+    {length: 10},
+    () => (Math.floor(Math.random() * 50) + 1) ** 2 // o + 1 é porque não quero que tenha números zero
+  );
+  const sum =  array.reduce((accumulator, elements) => accumulator + elements);
+
+  if (sum < 8000) {
+    return sum;
+  }
+  throw new Error();
+};
+
+const returnArrayDivision = (sum) => {
   const divisors = [2, 3, 5, 10];
-  return divisors.map((divisor) => result / divisor);
+  return divisors.map((divisor) => sum / divisor);
 };
 
-const sumElementsOfArray = (array) => {
-  return array.reduce((accumulator, elements) => accumulator + elements);
+const fetchPromise = async () => {
+  try {
+    const sum = await sumRandomNumbers();
+    const arrayWithDivisions = await returnArrayDivision(sum);
+
+    return console.log(arrayWithDivisions);
+  } catch (error) {
+    console.log('É mais de oito mil, Essa promise deve estar quebrada!');
+  }
 };
 
-const promise = new Promise((resolve, reject) => {
-  const arrayOfNumbers = [];
+fetchPromise();
 
-  for (let counter = 1; counter <= 10; counter += 1) {
-    const randomNumber = Math.floor(Math.random() * 50);
-    arrayOfNumbers.push(randomNumber ** 2);
-  }
-  const sumOfAllElements = sumElementsOfArray(arrayOfNumbers);
-
-  if (sumOfAllElements < 8000) {
-    resolve(returnArrayDivision(sumOfAllElements));
-  } else {
-    reject('É mais de oito mil! Essa promise deve estar quebrada!');
-  }
-})
-  .then((response) => response)
-  .then((responseOfResponse) => console.log(sumElementsOfArray(responseOfResponse)))
-  .catch((error) => console.log(error));
