80908fafc325ba71004be05c561d5ced2678e771
thomasravache <thomasravache31@gmail.com>
2021-07-20 18:35:24 -0300

    Exercicio de fixacao - OK

--- front-end/bloco01-introducao-a-react/dia1-hello-word-no-react/content/jsx/fixing-exercise/src/App.js
+++ front-end/bloco01-introducao-a-react/dia1-hello-word-no-react/content/jsx/fixing-exercise/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+const textJSX = <h1>Hello, jSX</h1>;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload {textJSX}.
         </p>
         <a
           className="App-link"
