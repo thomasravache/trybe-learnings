f493ea7ffd566fe75f88cefae51761f6e389921c
thomasravache <thomasravache31@gmail.com>
2021-07-20 19:04:41 -0300

    Exercicio de fixacao - OK

---
+++ front-end/bloco01-introducao-a-react/dia1-hello-word-no-react/content/classes-e-react/fixing-exercise/src/Component.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class MyComponent extends Component {
+  render() {
+    return(
+      <div className="fundo-preto">
+        <h1>Thomas Ravache</h1>
+        <p>Adoro programação!</p>
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
