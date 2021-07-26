2390f2831ad931bc79f6ad123b0958318a184036
thomasravache <thomasravache31@gmail.com>
2021-07-26 19:26:52 -0300

    Exercicio 02 da parte de transmissao de informacoes - OK

--- front-end/bloco12-componentes-com-estado-eventos-e-formularios-com-react/dia02-formularios-no-react/content/transmitindo-infos-comp-filho-para-pai-exercise/src/LoginInput.js
+++ front-end/bloco12-componentes-com-estado-eventos-e-formularios-com-react/dia02-formularios-no-react/content/transmitindo-infos-comp-filho-para-pai-exercise/src/LoginInput.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
 class LoginInput extends React.Component {
+  emailValidate(login) {
+    const isValid = login.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+    
+    if(!isValid || login.length === 0) {
+      return (
+        <ul>
+          <li>E-mail inválido.</li>
+        </ul>
+      );
+    }
+    return '';
+  }
+
   render() {
-    const { login, onChange } = this.props;
+    const { value, onChange } = this.props;
 
     return(
       <label htmlFor="">
         Login
-        <input type="text" name="login" value={ login } onChange={ onChange } />
+        <input type="text" name="login" value={ value } onChange={ onChange } />
+        <div className="error">{ this.emailValidate(value) }</div>
       </label>
     );
   }
