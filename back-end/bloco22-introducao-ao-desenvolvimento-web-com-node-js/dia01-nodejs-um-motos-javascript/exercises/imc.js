6f1f530522210d86230165464361b4c3d124effc
thomasravache <thomasravache31@gmail.com>
2022-01-07 19:06:14 -0300

    Exercicio 01 - OK

---
+++ back-end/bloco22-introducao-ao-desenvolvimento-web-com-node-js/dia01-nodejs-um-motos-javascript/exercises/imc.js
@@ -0,0 +1,3 @@
+const ImcCalculator = (height, weight) => (weight / Math.pow(height, 2)).toFixed(2);
+
+module.exports = ImcCalculator;
---
+++ back-end/bloco22-introducao-ao-desenvolvimento-web-com-node-js/dia01-nodejs-um-motos-javascript/exercises/index.js
@@ -0,0 +1,3 @@
+const imc = require('./imc');
+
+console.log(`seu imc é de ${imc(1.80, 60)}`);
