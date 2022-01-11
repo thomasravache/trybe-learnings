8fe0a7e5e6cd96ac997990f5534134c3a49dd3f3
thomasravache <thomasravache31@gmail.com>
2022-01-11 19:15:33 -0300

    Exercicio 04 - OK

--- bloco22/dia03-mocha-chai-e-sinon-testes-de-back-end-com-node-js/exercises/tests.js
+++ bloco22/dia03-mocha-chai-e-sinon-testes-de-back-end-com-node-js/exercises/tests.js
@@ -1,7 +1,10 @@
 const { expect } = require('chai');
 const numberStatus = require('./numberStatus');
+const fs = require('fs');
+const sinon = require('sinon');
+const myReadFileFunction = require('./readFile');
 
-describe('Exercício 1', () => {
+describe('Exercício de Status de um número', () => {
   it('Verifica se o parâmetro é um número', () => {
     const parameter = 'teste'
     const result = numberStatus(parameter);
@@ -25,5 +28,47 @@ describe('Exercício 1', () => {
     const result = numberStatus(0);
 
     expect(result).to.be.equal('neutro');
+  });
+});
+
+describe('Testando a chamada da função "readFile"', () => {
+  const FILE_NAME = 'arquivo.txt';
+  const FILE_CONTENT = 'Um texto qualquer';
+
+  describe('Caso a função consiga ler o arquivo', () => {
+    before(() => {
+      sinon.stub(fs.promises, 'readFile').resolves(FILE_CONTENT);
+    })
+
+    it('Verifica se retorna uma string', async () => {
+      const result = await myReadFileFunction(FILE_NAME, FILE_CONTENT);
+
+      expect(result).to.be.a('string');
+    });
+    it('Verifica se após a leitura retorna um "ok"', () => {
+      const result = await myReadFileFunction(FILE_NAME, FILE_CONTENT);
+
+      expect(result).to.be.equal('ok');
+    })
+
+    after(() => {
+      fs.promises.readFile.restore();
+    })
+  });
+
+  describe('Caso a função não consiga ler o arquivo', () => {
+    before(() => {
+      sinon.stub(fs.promises, 'readFile').rejects();
+    });
+
+    it('Verifica se retorna a string "Deu ruim"', async () => {
+      const result = await myReadFileFunction(FILE_NAME, FILE_CONTENT);
+
+      expect(result).to.be.equal('Deu ruim');
+    });
+
+    after(() => {
+      fs.promises.readFile.restore();
+    })
   });
 });
