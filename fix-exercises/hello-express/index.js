5e96afd67590c888c358a8534cb5eb52d1e0cd15
thomasravache <thomasravache31@gmail.com>
2022-01-13 10:11:47 -0300

    Exercicio de fixacao

--- fix-exercises/hello-express/index.js
+++ fix-exercises/hello-express/index.js
@@ -1,13 +1,31 @@
-const express = require('express');
+// const express = require('express');
 
+// const app = express();
+
+// app.get('/hello', handleHelloWorldRequest);
+
+// app.listen(3001, () => {
+//   console.log('Ouvindo na porta 3001');
+// });
+
+// function handleHelloWorldRequest(req, res) {
+//   res.status(200).send('Hello Worasald!');
+// }
+
+/* index.js */
+const express = require('express');
 const app = express();
 
-app.get('/hello', handleHelloWorldRequest);
+const recipes = [
+  { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
+  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
+  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
+];
 
-app.listen(3001, () => {
-  console.log('Ouvindo na porta 3001');
+app.get('/recipes', function (req, res) {
+  res.json(recipes);
 });
 
-function handleHelloWorldRequest(req, res) {
-  res.status(200).send('Hello World!');
-}
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na porta 3001');
+});
