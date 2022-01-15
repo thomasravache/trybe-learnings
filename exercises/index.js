dfe2395787c1cd57ba9f604226a201aa8b05492a
thomasravache <thomasravache31@gmail.com>
2022-01-14 23:57:15 -0300

    Exercicio 01

---
+++ exercises/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/ping', (_req, res) => {
+  res.status(200).json({ message: 'pong' });
+});
+
+app.listen(3001, () => {
+  console.log('Ouvindo na porta 3001');
+});
