4455d6aca4e3bf9e44bdeef7b68da30a6c87b57a
thomasravache <thomasravache31@gmail.com>
2022-01-21 11:18:51 -0300

    Arrumando identaçao do middleware

--- exercises/middlewares/userMiddleware.js
+++ exercises/middlewares/userMiddleware.js
@@ -4,27 +4,27 @@ const router = express.Router();
 const User = require('../models/userModel');
 
 router.post('/', async (req, res, next) => {
-try {
-  const { firstName, lastName, email, password } = req.body;
+  try {
+    const { firstName, lastName, email, password } = req.body;
 
-  const newUser = await User.create(firstName, lastName, email, password);
+    const newUser = await User.create(firstName, lastName, email, password);
 
-  const result = {
-    id: newUser.insertId,
-    firstName,
-    lastName,
-    email,
-    password,
-  };
+    const result = {
+      id: newUser.insertId,
+      firstName,
+      lastName,
+      email,
+      password,
+    };
 
-  return res.status(201).json(result);
-} catch (e) {
-  if(e.error === true) return res.status(422).json(e);
-  return next(e);
-}
+    return res.status(201).json(result);
+  } catch (e) {
+    if(e.error === true) return res.status(422).json(e);
+    return next(e);
+  }
 });
 
-// ---------- MIDDLEWARE DE TRATAMENTO DE ERRO DE APLICAÇÃO ------------
+// ---------- MIDDLEWARE PARA TRATAMENTO DE ERROS DE APLICAÇÃO ------------
 
 router.use((err, _req, res, _next) => {
   console.error(err);
