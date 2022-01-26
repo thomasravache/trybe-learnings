cad4c991b1efbfafa4b748ede17f9a16c212da69
thomasravache <thomasravache31@gmail.com>
2022-01-26 15:19:56 -0300

    Renomeando rota ping

--- exercises/cep-lookup/controllers/PingController.js
+++ exercises/cep-lookup/controllers/PingController.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const pingRouter = express.Router();
 
-pingRouter.get('/', (req, res) => {
+pingRouter.get('/', (_req, res) => {
   res.status(200).json({ message: 'pong!' });
 });
 
