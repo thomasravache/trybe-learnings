f77e3930afa9e4aacd96eaae0fdf6e83ab087c8a
thomasravache <thomasravache31@gmail.com>
2022-01-26 15:21:29 -0300

    Ajustando rotas no index.js

--- exercises/cep-lookup/index.js
+++ exercises/cep-lookup/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-const pingRouter = require('./controllers/ping');
+const pingRouter = require('./controllers/PingController');
+const cepRouter = require('./controllers/CepController');
+const ErrorController = require('./controllers/ErrorController');
 
 const PORT = process.env.PORT;
 
@@ -11,6 +13,8 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/ping', pingRouter);
+app.use('/cep', cepRouter);
+app.use(ErrorController);
 
 app.listen(PORT, () => {
   console.log(`Ouvindo na porta ${PORT}`);
