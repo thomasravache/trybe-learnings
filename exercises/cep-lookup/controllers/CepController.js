bc1b40153283bad4b59a757ff5dc89f7715233a3
thomasravache <thomasravache31@gmail.com>
2022-01-26 17:42:57 -0300

    Finalizando cep controller

--- exercises/cep-lookup/controllers/CepController.js
+++ exercises/cep-lookup/controllers/CepController.js
@@ -1,21 +1,41 @@
 const express = require('express');
 const cepRouter = express.Router();
 const rescue = require('express-rescue');
+const Joi = require('joi');
 const CepService = require('../services/CepService');
 
 cepRouter.get('/:cep', rescue(async (req, res) => {
   const { cep } = req.params;
 
-  const isValidCep = /\d{5}-?\d{3}/i;
-
-  if (!isValidCep.test(cep)) throw {
-    code: 'invalidData',
-    message: 'CEP inválido',
-  };
-
-  const searchedCep = await CepService.getByCepName(cep);
+  const searchedCep = await CepService.getByCepNumber(cep);
 
   return res.status(200).json(searchedCep);
+}));
+
+cepRouter.post('/', rescue(async (req, res, next) => {
+  const { cep, logradouro, bairro, localidade, uf } = req.body;
+
+  const { error } = Joi.object({
+    cep: Joi.string().not().empty().required(),
+    logradouro: Joi.string().not().empty().required(),
+    bairro: Joi.string().not().empty().required(),
+    localidade: Joi.string().not().empty().required(),
+    uf: Joi.string().not().empty().required(),
+  }).validate({ cep, logradouro, bairro, localidade, uf });
+
+  if (error) return next(error);
+
+  await CepService.create(cep, logradouro, bairro, localidade, uf);
+
+  const result = {
+    cep,
+    logradouro,
+    bairro,
+    localidade,
+    uf,
+  };
+
+  res.status(201).json(result);
 }));
 
 module.exports = cepRouter;
