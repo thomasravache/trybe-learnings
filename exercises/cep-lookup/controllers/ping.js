98ddd02bf4374382fff4de749e0fd07b494e5495
thomasravache <thomasravache31@gmail.com>
2022-01-26 14:29:29 -0300

    Criando rota ping - Concluido

---
+++ exercises/cep-lookup/controllers/ping.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const pingRouter = express.Router();
+
+pingRouter.get('/', (req, res) => {
+  res.status(200).json({ message: 'pong!' });
+});
+
+module.exports = pingRouter;
--- exercises/cep-lookup/index.js
+++ exercises/cep-lookup/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
+const pingRouter = require('./controllers/ping');
+
 const PORT = process.env.PORT;
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use('/ping', pingRouter);
 
 app.listen(PORT, () => {
   console.log(`Ouvindo na porta ${PORT}`);
