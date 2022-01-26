ef7d6c62948605578ef0de182a9a53cf1078be8b
thomasravache <thomasravache31@gmail.com>
2022-01-26 17:43:37 -0300

    Finalizando CepService

--- exercises/cep-lookup/services/CepService.js
+++ exercises/cep-lookup/services/CepService.js
@@ -1,21 +1,42 @@
 const CepModel = require('../models/CepModel');
 
+const isValidCep = (cep) => {
+  const isValidCep = /\d{5}-\d{3}/;
+
+  if (cep.length !== 9) return false;
+
+  return isValidCep.test(cep) ? true : false;
+}
+
 const getAll = async () => {
   const ceps = await CepModel.getAll();
 
   return ceps;
 };
 
-const getByCepName = async (name) => {
-  const cep = await CepModel.getByCepName(name);
-  console.log(cep);
+const getByCepNumber = async (cepNumber) => {
+  if (!isValidCep(cepNumber)) throw { code: 'invalidData', message: 'CEP inválido' };
+  
+  const cep = await CepModel.getByCepNumber(cepNumber);
 
   if (cep.length === 0) throw { code: 'notFound', message: 'CEP não encontrado' };
 
   return cep[0];
 };
 
+const create = async (cep, logradouro, bairro, localidade, uf) => {
+  if (!isValidCep(cep)) throw { code: 'invalidData', message: 'CEP inválido' };
+
+  const allCeps = await CepModel.getAll();
+  const alreadyExists = allCeps.some((data) => data.cep === cep);
+
+  if (alreadyExists) throw { code: 'alreadyExists', message: 'CEP já existente' };
+
+  return await CepModel.create(cep, logradouro, bairro, localidade, uf);
+};
+
 module.exports = {
   getAll,
-  getByCepName,
+  getByCepNumber,
+  create,
 };
