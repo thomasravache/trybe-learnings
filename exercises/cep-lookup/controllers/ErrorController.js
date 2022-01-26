bbe854f177d392092949d04782144ec53f2d1eb1
thomasravache <thomasravache31@gmail.com>
2022-01-26 17:43:10 -0300

    Finalizando ErrorController

--- exercises/cep-lookup/controllers/ErrorController.js
+++ exercises/cep-lookup/controllers/ErrorController.js
@@ -1,17 +1,28 @@
 module.exports = (err, _req, res, _next) => {
+  if (err.isJoi) return res.status(400).json({
+    error: {
+      code: 'invalidData',
+      message: err.details[0].message,
+    }
+  })
+  
   const statusCodes = {
     invalidData: 400,
     notFound: 404,
+    alreadyExists: 409,
   };
 
   const statusCode = statusCodes[err.code];
 
-  if (!statusCode) return res.status(500).json({
-    error: {
-      code: 'internalServerError',
-      message: 'Erro inesperado!',
-    }
-  });
+  if (!statusCode) {
+      console.error(err);
+      return res.status(500).json({
+        error: {
+          code: 'internalServerError',
+          message: 'Erro inesperado!',
+        }
+      });
+  }
 
   return res.status(statusCode).json({
     error: {
