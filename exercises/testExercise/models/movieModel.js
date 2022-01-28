19e984e077e3b938a5cce58238a199d305b2c376
thomasravache <thomasravache31@gmail.com>
2022-01-28 19:17:38 -0300

    ✅ Adicionando getById + testes na camada de model

--- exercises/testExercise/models/movieModel.js
+++ exercises/testExercise/models/movieModel.js
@@ -1,5 +1,13 @@
 const connection = require('./connection');
 
+const serialize = (movie) => {
+  return {
+    title: movie.title,
+    directedBy: movie.directed_by,
+    releaseYear: movie.release_year,
+  }
+};
+
 const create = async ({ title, directedBy, releaseYear }) => {
   const query = `
     INSERT INTO movies (title, directed_by, release_year)
@@ -13,6 +21,17 @@ const create = async ({ title, directedBy, releaseYear }) => {
   };
 };
 
+const getById = async ({ id }) => {
+  const query = `
+    SELECT title, directed_by, release_year FROM movies WHERE id = ?
+  `;
+
+  const [result] = await connection.execute(query, [id]);
+
+  return result.map(serialize);
+};
+
 module.exports = {
   create,
+  getById,
 };
--- exercises/testExercise/tests/models/movieModel.test.js
+++ exercises/testExercise/tests/models/movieModel.test.js
@@ -5,7 +5,7 @@ const MoviesModel = require('../../models/movieModel');
 const connection = require('../../models/connection');
 
 describe('----- TESTES DA CAMADA DE MODEL -----\n', () => {
-  describe('Insere um novo filme no BD', () => {
+  describe('(create) Insere um novo filme no BD', () => {
     const payloadMovie = {
       title: 'Example Movie',
       directedBy: 'Jane Dow',
@@ -37,5 +37,62 @@ describe('----- TESTES DA CAMADA DE MODEL -----\n', () => {
       });
   
     });
-  })
+  });
+
+  describe('(getById) Procura um filme no BD', () => {
+    describe('quando o filme não é encontrado', () => {
+      const ID_EXAMPLE = 1;
+  
+      before(async () => {
+        sinon.stub(connection, 'execute').resolves([[]])
+      })
+  
+      after(async () => {
+        connection.execute.restore();
+      })
+  
+      it('quando o filme não é encontrado', async () => {
+        const response = await MoviesModel.getById(ID_EXAMPLE);
+
+        expect(response).to.be.an('array');
+        expect(response.length).to.be.equal(0);
+      });
+    });
+
+    describe('quando o filme é encontrado', () => {
+      const ID_EXAMPLE = 1;
+
+      before(async () => {
+        const payload = {
+          title: 'Teste',
+          directedBy: 'Teste',
+          releaseYear: 1970,
+        };
+
+        sinon.stub(connection, 'execute').resolves([[payload]]);
+      });
+
+      after(async () => {
+        connection.execute.restore();
+      });
+
+      it('deve retornar um array', async () => {
+        const response = await MoviesModel.getById(ID_EXAMPLE);
+  
+        expect(response).to.be.an('array');
+      });
+
+      it('deve conter apenas um objeto', async () => {
+        const response = await MoviesModel.getById(ID_EXAMPLE);
+
+        expect(response.length).to.be.equal(1);
+      });
+
+      it('deve conter as chaves "title", "directedBy", "releaseYear"', async () => {
+        const [response] = await MoviesModel.getById(ID_EXAMPLE);
+
+        expect(response).to.have.keys('title', 'directedBy', 'releaseYear');
+      });
+    });
+  });
 });
