e84b1875d6cf853a5c14b2a462e88ef8890d41f0
thomasravache <thomasravache31@gmail.com>
2022-01-30 00:26:05 -0300

    Testes + camada de controller

--- exercises/testExercise/controllers/movieController.js
+++ exercises/testExercise/controllers/movieController.js
@@ -3,6 +3,21 @@ const movieRouter = express.Router();
 
 const MoviesService = require('../services/movieService');
 
+const errorHandler = (error, res) => {
+  const statusCodes = {
+    notFound: 404,
+  };
+
+  const statusCode = statusCodes[error.code];
+
+  if (!statusCode) {
+    console.error(error);
+    return res.status(500).json({ message: 'Erro inesperado!' });
+  };
+
+  return res.status(statusCode).json({ message: error.message });
+};
+
 movieRouter.post('/', async (req, res) => {
   const { title, directedBy, releaseYear } = req.body;
 
@@ -13,9 +28,16 @@ movieRouter.post('/', async (req, res) => {
   return res.status(201).send('Filme criado com sucesso!');
 });
 
-movieRouter.get('/:teste', (req, res) => {
-  const { teste } = req.params;
-  res.status(200).send(teste);
-})
+movieRouter.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+  
+    const result = await MoviesService.getById(id);
+  
+    return res.status(200).json(result);
+  } catch (e) {
+    errorHandler(e, res);
+  }
+});
 
 module.exports = movieRouter;
--- exercises/testExercise/tests/controllers/movieController.test.js
+++ exercises/testExercise/tests/controllers/movieController.test.js
@@ -5,10 +5,15 @@ const MoviesService = require('../../services/movieService');
 
 const MoviesController = {
   create: MoviesRouter.stack[0].route.stack[0].handle,
+  getById: MoviesRouter.stack[1].route.stack[0].handle,
 }
 
+// const ErrorController = {
+//   treatError = require('../../controllers/ErrorController'),
+// }
+
 describe('----- TESTES DA CAMADA DE CONTROLLER -----\n', () => {
-  describe('Ao chamar o controller de create', () => {
+  describe('--- (create) --- Ao chamar o controller de create', () => {
     describe('quando o payload informado não é válido', () => {
       const req = {};
       const res = {};
@@ -32,7 +37,7 @@ describe('----- TESTES DA CAMADA DE CONTROLLER -----\n', () => {
         expect(res.status.calledWith(400)).to.be.equal(true);
       });
 
-      it('é chamado o sendo com a mensagem "Dados inválidos"', async () => {
+      it('é chamado o send com a mensagem "Dados inválidos"', async () => {
         await MoviesController.create(req, res);
 
         expect(res.send.calledWith('Dados inválidos')).to.be.equal(true);
@@ -73,4 +78,83 @@ describe('----- TESTES DA CAMADA DE CONTROLLER -----\n', () => {
       });
     });
   });
-})
+
+  describe('--- (getById) --- Ao chamar o controller getById', () => {
+    describe('quando o payload não é válido', () => {
+      const req = {};
+      const res = {};
+
+      before(() => {
+        req.params = {};
+
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns();
+
+        sinon.stub(MoviesService, 'getById').rejects({ code: 'notFound', message: 'Filme não encontrado' });
+      });
+
+      after(() => {
+        MoviesService.getById.restore();
+      });
+
+      it('Devolve status "404" para o cliente', async () => {
+        await MoviesController.getById(req, res);
+
+        expect(res.status.calledWith(404)).to.be.true;
+      });
+
+      it('Devolve json com message: "Filme não encontrado"', async () => {
+        await MoviesController.getById(req, res);
+
+        expect(res.json.calledWith({ message: 'Filme não encontrado' })).to.be.true;
+      });
+    });
+
+    describe('quando o payload é válido', () => {
+      const ID_EXAMPLE = 1;
+      const req = {};
+      const res = {};
+
+      before(() => {
+        req.params = {
+          id: ID_EXAMPLE,
+        };
+
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns();
+
+        sinon.stub(MoviesService, 'getById').resolves([
+          {
+            title: 'Teste',
+            directedBy: 'Teste',
+            releaseYear: 1970,
+          },
+        ]);
+      });
+
+      after(() => {
+        MoviesService.getById.restore();
+      });
+
+      it('Devolve o status "200"', async () => {
+        await MoviesController.getById(req, res);
+
+        expect(res.status.calledWith(200)).to.be.true;
+      });
+
+      it('Devolve o json com a informação do movie', async () => {
+        await MoviesController.getById(req, res);
+
+        const jsonReturn = [
+          {
+            title: 'Teste',
+            directedBy: 'Teste',
+            releaseYear: 1970,
+          },
+        ];
+
+        expect(res.json.calledWith(jsonReturn)).to.be.true;
+      });
+    });
+  })
+});
