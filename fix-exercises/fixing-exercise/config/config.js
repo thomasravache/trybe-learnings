6dbbf06ca57d50b64f4f9e3e9487442e9b8fa2ce
thomasravache <thomasravache31@gmail.com>
2022-02-10 15:16:04 -0300

    Fixacao do conteudo ate 'Utilizando relacionamentos'

---
+++ fix-exercises/fixing-exercise/config/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const config = {
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    dialect: "mysql"
+  },
+  test: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    dialect: "mysql"
+  },
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    dialect: "mysql"
+  }
+};
+
+module.exports = config;
---
+++ fix-exercises/fixing-exercise/models/Address.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const Address = sequelize.define('Address', {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    city: DataTypes.STRING,
+    street: DataTypes.STRING,
+    number: DataTypes.INTEGER,
+    employeeId: { type: DataTypes.INTEGER, foreignKey: true },
+  }, {
+    underscored: true,
+    timestamps: false,
+    tableName: 'Adresses'
+  });
+
+  Address.associate = (models) => {
+    Address.belongsTo(models.Employee, {
+      foreignKey: 'employee_id', as: 'employees'
+    });
+  };
+
+  return Address;
+};
