7199ccdd127d5be0a0dd7a2dc20eeee3bcbb138b
thomasravache <thomasravache31@gmail.com>
2022-02-16 10:35:51 -0300

    Implementando jwt

---
+++ api/auth/validateJWT.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+const { User } = require('../../models');
+
+const segredo = 'seusecretdetoken';
+
+module.exports = async (req, res, next) => {
+  const token = req.headers['authorization'];
+
+  if (!token) return res.status(401).json({ error: 'Token não encontrado' });
+
+  try {
+    const decoded = jwt.verify(token, segredo);
+
+    const user = await User.findOne({ where: { username: decoded.data.username } });
+
+    if (!user) return res.status(401).json({ message: 'Erro ao procurar usuário do token' });
+
+    req.user = user;
+
+    next();
+  } catch (e) {
+    return res.status(401).json({ message: e.message });
+  }
+};
