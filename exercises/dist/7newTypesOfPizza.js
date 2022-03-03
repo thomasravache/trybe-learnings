d7673c6ec5eed1d32bcfcb57c38e853589952543
thomasravache <thomasravache31@gmail.com>
2022-03-03 20:19:13 -0300

    Exercicio 07

--- exercises/dist/7newTypesOfPizza.js
+++ exercises/dist/7newTypesOfPizza.js
@@ -3,3 +3,27 @@ Object.defineProperty(exports, "__esModule", { value: true });
 ;
 ;
 ;
+const commonPizza1 = {
+    flavor: 'Calabresa',
+    slices: 4
+};
+const commonPizza2 = {
+    flavor: 'Frango',
+    slices: 6,
+};
+const commonPizza3 = {
+    flavor: 'Pepperoni',
+    slices: 8,
+};
+const vegetarianPizza1 = {
+    flavor: 'Cogumelo',
+    slices: 4,
+};
+const vegetarianPizza2 = {
+    flavor: 'Marguerita',
+    slices: 6,
+};
+const sweetPizza1 = {
+    flavor: 'Goiabada com Queijo',
+    slices: 4,
+};
