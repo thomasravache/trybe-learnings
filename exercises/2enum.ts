faf6332b4dff76d7cac3b3d2bbd6f989607ddae5
thomasravache <thomasravache31@gmail.com>
2022-03-03 16:48:39 -0300

    Exercicio 02

---
+++ exercises/2enum.ts
@@ -0,0 +1,10 @@
+enum Season {
+  Autumn = 'Outono',
+  Winter = 'Inverno',
+  Spring = 'Primavera',
+  Summer = 'Verão',
+}
+
+const season: Season = Season.Autumn;
+
+console.log(season);
---
+++ exercises/dist/2enum.js
@@ -0,0 +1,10 @@
+"use strict";
+var Season;
+(function (Season) {
+    Season["Autumn"] = "Outono";
+    Season["Winter"] = "Inverno";
+    Season["Spring"] = "Primavera";
+    Season["Summer"] = "Ver\u00E3o";
+})(Season || (Season = {}));
+const season = Season.Autumn;
+console.log(season);
