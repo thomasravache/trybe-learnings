01210bd801e2ac50582703aafd1058a2583db98a
thomasravache <thomasravache31@gmail.com>
2022-03-14 03:23:05 -0300

    Exercicio Fixacao 01.2

--- fix-exercises/exercise01.ts
+++ fix-exercises/exercise01.ts
@@ -1,4 +1,19 @@
 abstract class Character {
+
+  constructor(public name: string) {  }
+
   abstract talk(): void;
+
   abstract specialMove(): void;
 }
+
+class MeleeCharacter extends Character {
+  public talk(): void {
+    console.log(`${this.name} está falando.`)
+  }
+
+  public specialMove(): void {
+    console.log(`${this.name} está usando o Ultimate!`);
+  }
+}
+
