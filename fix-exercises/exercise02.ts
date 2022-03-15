8958b795778644f5f011cb54c431f8ee621f1a21
thomasravache <thomasravache31@gmail.com>
2022-03-15 15:07:35 -0300

    Exercicio fixacao 02.2

--- fix-exercises/exercise02.ts
+++ fix-exercises/exercise02.ts
@@ -7,11 +7,40 @@ interface DbCharacter extends Character {
   id: number;
 }
 
+
 interface IModel {
   create(character: Character): void;
-  read(): Character;
-  update(newCharacter: Character): void;
-  remove(character: Character): void;
+  read(): DbCharacter[];
+  update(character: DbCharacter): DbCharacter;
+  remove(id: number): void;
+}
+
+class LocalDbModel implements IModel {
+  constructor(private db: DbCharacter[]) {  }
+  
+  public create(character: Character): void {
+    this.db.push({ id: this.db.length + 1, name: character.name, specialMove: character.specialMove })
+  }
+  
+  public read(): DbCharacter[] {
+    return this.db;
+  }
+  
+  public update(character: DbCharacter): DbCharacter {
+    const characterIndex = this.db.findIndex((char) => char.id === character.id);
+    
+    if (characterIndex === -1) throw new Error('character not found');
+    
+    this.db.splice(characterIndex, 1, character);
+    
+    return character;
+  }
+  
+  public remove(id: number): void {
+    const characterIndex = this.db.findIndex((character) => character.id === id);
+    
+    this.db.splice(characterIndex, 1);
+  }
 }
 
 const db: DbCharacter[] = [];
