24157284f08ff097531fd03678c213aa73240be8
thomasravache <thomasravache31@gmail.com>
2022-03-19 01:50:22 -0300

    Exemplo 02 - OK

---
+++ fix-exercises/src/index.ts
@@ -0,0 +1,56 @@
+// ./src/index.ts
+
+import { Connector, RedisConnector } from "./Connectors";
+
+const token = 'ce42033d-9133-457a-a1a1-41ac0b63a333';
+const conn = new RedisConnector({
+  host: 'redisdb',
+  port: 6379});
+
+// O resto do index.ts continua igualzinho 😎
+
+const main = async (connector: Connector) => {
+  connector.firstCount(token);
+
+  const logCount = async () => {
+    try {
+      const count = await connector.getCount(token);
+      console.log(count);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const doSomeIncrements = () => {
+    logCount();
+    connector.incrementCount(token);
+    connector.incrementCount(token);
+    connector.incrementCount(token);
+  };
+
+  const incrementsInterval = setInterval(doSomeIncrements, 500);
+
+  setTimeout(() => {
+    clearInterval(incrementsInterval);
+    logCount();
+    connector.clearCount(token);
+    connector.closeConnection();
+  }, 5500);
+};
+
+main(conn);
+
+/*
+Saída:
+0
+3
+6
+9
+12
+15
+18
+21
+24
+27
+30
+*/
