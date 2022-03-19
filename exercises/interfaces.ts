1fe065fb38bb2d6d397e43a15f2da9fd2b110839
thomasravache <thomasravache31@gmail.com>
2022-03-19 09:30:39 -0300

    feat: necessary archives
    necessary archives to implement next exercises

--- exercises/interfaces.ts
+++ exercises/interfaces.ts
@@ -2,3 +2,9 @@ export interface IFooCepAPI {
   getAddressByCEP(cep: string, number: number): Promise<string>;
   getCepByAddress(address: string, number:number): Promise<string>
 }
+
+// ./interfaces.ts
+export interface IVehicle {
+  drive(): void;
+  fly(): void;
+}
